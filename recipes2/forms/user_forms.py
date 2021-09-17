7786b21686772bde0482a0e6f51c291e5affba59
SeraphDev6 <wesley.giles@gmail.com>
2021-09-17 09:48:36 -0400

    misc bugfixes

--- recipes2/forms/user_forms.py
+++ recipes2/forms/user_forms.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
-from recipes2.models import User
+from recipes2.models.user import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
