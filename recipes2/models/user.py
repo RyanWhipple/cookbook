17160c502022329f4d26880cf4954b0fa96627df
SeraphDev6 <82985907+SeraphDev6@users.noreply.github.com>
2021-09-17 09:33:04 -0400

    Update user.py

--- recipes2/models/user.py
+++ recipes2/models/user.py
@@ -34,19 +34,3 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-<<<<<<< Updated upstream:recipes2/models.py
-
-
-class Recipe(db.Model):
-    id                  = db.Column(db.Integer, primary_key=True)
-    title               = db.Column(db.String(100), nullable=False)
-    short_description   = db.Column(db.String(255), nullable=False)
-    image_file          = db.Column(db.String(20), nullable=False, default='default.jpg')
-    date_posted         = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content             = db.Column(db.Text, nullable=False)
-    user_id             = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-=======
->>>>>>> Stashed changes:recipes2/models/user.py
