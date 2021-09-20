67ae1755088b04be6596ae7b98c52e0d6ba3c299
Ryan Whipple <ryan_whipple@icloud.com>
2021-09-20 14:43:40 -0700

    user and recipe pics save to their own folders

--- recipes2/utils/utils.py
+++ recipes2/utils/utils.py
@@ -6,11 +6,11 @@ from flask_mail import Message
 from recipes2 import mail
 
 
-def save_picture(form_picture):
+def save_picture(form_picture, folder):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, f'static/{folder}', picture_fn)
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail=output_size
