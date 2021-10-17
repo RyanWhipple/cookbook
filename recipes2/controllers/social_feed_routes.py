8e1dbf2ac50599415bc01ab7401bafcf1a09e012
SeraphDev6 <wesley.giles@gmail.com>
2021-10-17 17:43:35 -0400

    Finalized algorithm, added view recipes button to results card in social feed

--- recipes2/controllers/social_feed_routes.py
+++ recipes2/controllers/social_feed_routes.py
@@ -3,12 +3,10 @@ from flask import Blueprint
 from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import current_user, login_required
 from recipes2 import db
-import recipes2
+from datetime import datetime,timedelta
 from recipes2.models.recipe import Recipe
 from recipes2.models.result import Result
 from recipes2.models.user import Follower
-from recipes2.forms.recipe_forms import RecipeForm
-from recipes2.forms.result_forms import ResultForm
 from recipes2.utils.utils import save_picture
 from sqlalchemy import delete
 
@@ -30,36 +28,9 @@ def social_feed():
     results = Result.query.order_by(Result.created_at.desc()).filter(Result.recipe_id.in_([recipe.id for recipe in recipes]))
     following = [ _.followee_id for _ in current_user.follower_id]
     # print("recipes: ", recipes)
-    result_list = [ _ for _ in recipes]+[ _ for _ in results]
-    result_list.sort(key=lambda x: x.created_at, reverse=True)
-    print(result_list)
-    
-
-
-    items_to_show = []
-    recipe_index = 0
-    result_index = 0
-    num_of_recipes = recipes.count()
-    num_of_results = results.count()
-    recipes_left = num_of_recipes > 0
-    results_left = num_of_results > 0
-    
-    while len(items_to_show) < 10:
-        if results_left and results[result_index].created_at > recipes[recipe_index].created_at:
-            items_to_show.append([results[result_index], "result"])
-            result_index += 1
-            if result_index == num_of_results:
-                print("Ran out of results!")
-                results_left = False
-        elif recipes_left:
-            items_to_show.append([recipes[recipe_index], "recipe"])
-            recipe_index += 1
-            if recipe_index == num_of_recipes:
-                print("Ran out of recipes!")
-                recipes_left = False
-        if not recipes_left and not results_left:
-            print("Ran out of both results and recipes!")
-            break
+    items_to_show = [ _ for _ in recipes]+[ _ for _ in results]
+    items_to_show.sort(key=lambda x: x.created_at, reverse=True)
+    items_to_show.sort(key=lambda x: 0 if (x.user_id in following and datetime.now() - x.created_at < timedelta(days=3)) else 1)    
 
     page = request.args.get(get_page_parameter(), type=int, default=1)
     per_page = 5
