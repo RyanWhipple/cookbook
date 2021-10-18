c8b9e99b051f74541b567cd231ca73c6718634d8
SeraphDev6 <wesley.giles@gmail.com>
2021-10-18 10:04:50 -0400

    finished search bar routes and added comments for search bar functionality

--- recipes2/controllers/searchbar_routes.py
+++ recipes2/controllers/searchbar_routes.py
@@ -1,18 +1,70 @@
 from flask import Blueprint
 from flask import render_template, request
+from flask_login import current_user
 from recipes2 import db
 from recipes2.models.recipe import Recipe
-from recipes2.models.result import Result
+from flask_paginate import Pagination, get_page_parameter
+
 
 searchbar = Blueprint("searchbar",__name__)
 
-@searchbar.route('/search')
+@searchbar.route('/search', methods=["GET"])
 def search():
+    # search by the get request
     search_query = request.args.get("q")
     search_by = "%{}%".format(search_query)
-    results =[]
-    results.push(Recipe.query.filter(Recipe.name.like(search_by)))
-    results.push(Recipe.query.filter(Recipe.description.like(search_by)))
-    results.push(Recipe.query.filter(Recipe.instructions.like(search_by)))
-    results.push(Recipe.query.filter(Recipe.ingredients.like(search_by)))
-    return render_template("search_page.html",results=results)
+    results =list(Recipe.query.filter(Recipe.name.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.description.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.ingredients.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.instructions.like(search_by)))
+    # get only public recipes
+    filtered = list(filter(lambda result: result.public == 1, results))
+    # remove duplicates from list
+    filtered = list(set(filtered))
+    #if user is logged in show the recipes of users they are following first
+    if current_user.is_authenticated:
+        following = [ _.followee_id for _ in current_user.follower_id]
+        filtered.sort(key=lambda x: 0 if x.user_id in following else 1)
+    # pagination was copied and pasted from social feed
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    per_page = 12
+    offset = (page-1) * per_page
+    total = len(filtered)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    filtered = filtered[offset : offset + per_page]
+    return render_template("search_page.html",recipes=filtered,
+                            page=page,
+                            per_page=per_page,
+                            pagination=pagination,
+                            title="Search Recipes")
+
+@searchbar.route('/ajax/search', methods=["GET"])
+def ajax_search():
+    # search by the get request
+    search_query = request.args.get("q")
+    search_by = "%{}%".format(search_query)
+    results =list(Recipe.query.filter(Recipe.name.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.description.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.ingredients.like(search_by))) +\
+    list(Recipe.query.filter(Recipe.instructions.like(search_by)))
+    # get only public recipes
+    filtered = list(filter(lambda result: result.public == 1 or (current_user.is_authenticated and current_user.id == result.user_id), results))
+    # remove duplicates from list
+    filtered = list(set(filtered))
+    #if user is logged in show the recipes of users they are following first
+    if current_user.is_authenticated:
+        following = [ _.followee_id for _ in current_user.follower_id]
+        filtered.sort(key=lambda x: 0 if x.user_id in following else 1)
+    # pagination was copied and pasted from social feed
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    per_page = 12
+    offset = (page-1) * per_page
+    total = len(filtered)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    filtered = filtered[offset : offset + per_page]
+    return render_template("partials/search.html",recipes=filtered,
+                            page=page,
+                            per_page=per_page,
+                            pagination=pagination)
--- recipes2/controllers/social_feed_routes.py
+++ recipes2/controllers/social_feed_routes.py
@@ -1,17 +1,13 @@
 import re
 from flask import Blueprint
-from flask import render_template, url_for, flash, redirect, request, abort
+from flask import render_template, url_for, redirect, request
 from flask_login import current_user, login_required
 from recipes2 import db
 from datetime import datetime,timedelta
 from recipes2.models.recipe import Recipe
 from recipes2.models.result import Result
 from recipes2.models.user import Follower
-from recipes2.utils.utils import save_picture
-from sqlalchemy import delete
-
 from flask_paginate import Pagination, get_page_parameter
-from sqlalchemy import create_engine
 
 
 social_feeds = Blueprint('social_feed', __name__)
@@ -23,14 +19,17 @@ social_feeds = Blueprint('social_feed', __name__)
 @login_required
 def social_feed():
     
-    
+    # only get public recipes
     recipes = Recipe.query.order_by(Recipe.created_at.desc()).filter_by(public=1)
+    # only get results from public recipes
     results = Result.query.order_by(Result.created_at.desc()).filter(Result.recipe_id.in_([recipe.id for recipe in recipes]))
+    # get the list of users current_user is following
     following = [ _.followee_id for _ in current_user.follower_id]
-    # print("recipes: ", recipes)
-    items_to_show = [ _ for _ in recipes]+[ _ for _ in results]
+    # make the concatenated list
+    items_to_show = list(recipes)+list(results)
+    # (Simple) Sorting Algorithm
     items_to_show.sort(key=lambda x: x.created_at, reverse=True)
-    items_to_show.sort(key=lambda x: 0 if (x.user_id in following and datetime.now() - x.created_at < timedelta(days=3)) else 1)    
+    items_to_show.sort(key=lambda x: 0 if (x.user_id in following and datetime.now() - x.created_at < timedelta(days=3)) else 1)
 
     page = request.args.get(get_page_parameter(), type=int, default=1)
     per_page = 5
@@ -41,8 +40,6 @@ def social_feed():
 
     items_to_show = items_to_show[offset : offset + per_page]
 
-    
-    
     return render_template('social_feed.html',
                             items_to_show = items_to_show,
                             page=page,
@@ -55,31 +52,31 @@ def social_feed():
 @social_feeds.route('/social_feed/follow/<int:followee_id>', methods=['GET', 'POST'])
 @login_required
 def follow(followee_id):
-    
+
     follower = Follower(follower_id=current_user.id, followee_id=followee_id)
     db.session.add(follower)
     db.session.commit()
-    
+
     return redirect(url_for('main.home'))
 
 
 @social_feeds.route('/social_feed/unfollow/<int:followee_id>', methods=['GET', 'POST'])
 @login_required
 def unfollow(followee_id):
-    
+
     to_delete = Follower.query.filter(Follower.followee_id==followee_id, Follower.follower_id == current_user.id).first()
     db.session.delete(to_delete)
     db.session.commit()
-    
+
     return redirect(url_for('users.account'))
 
 
 @social_feeds.route('/social_feed/block/<int:follower_id>', methods=['GET', 'POST'])
 @login_required
 def block(follower_id):
-    
+
     to_delete = Follower.query.filter(Follower.follower_id==follower_id, Follower.followee_id == current_user.id).first()
     db.session.delete(to_delete)
     db.session.commit()
-    
+
     return redirect(url_for('users.account'))
