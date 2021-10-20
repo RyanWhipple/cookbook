86b19b90937c205bc83bbb04e293e50d00ad7c9d
SeraphDev6 <wesley.giles@gmail.com>
2021-10-20 19:54:10 -0400

    tried to update gmail settings for flask mail

--- recipes2/utils/utils.py
+++ recipes2/utils/utils.py
@@ -1,9 +1,8 @@
-import os
 import secrets
 from PIL import Image, ImageStat
 from flask import url_for, current_app
 from flask_mail import Message
-from recipes2 import mail
+from recipes2 import mail, app
 
 output_size = (500,500)
 
@@ -24,13 +23,12 @@ def save_picture(form_picture, folder):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Request', sender=os.environ['EMAIL_USER'], recipients=[user.email])
-    print("send_reset_token is sending an email to: ", user.email)
-    msg.body = f'''To reset your password, visit the following link:
+    msg = Message(subject='Password Reset Request', sender=app.config.get('EMAIL_USER'), recipients=[user.email],body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
-
 If you did not make this request please ignore this email and no changes will be made.
-'''
+''')
+    print("send_reset_token is sending an email to: ", user.email)
+
     mail.send(msg)
 
 def check_for_duplicate(picture,folder):
